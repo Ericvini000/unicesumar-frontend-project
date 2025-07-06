9233e4aac93e8d4e4dc0026eeef423695bab353a
Eric Vinicius <ericviniciusfelixsilva@gmail.com>
2025-07-06 20:13:29 -0300

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// 
+const postTitle = document.querySelector(".post__input--title")
+const postContent = document.querySelector(".post__input--content")
+const postButton = document.querySelector(".post__form--button")
+
+
+
+console.log(inputValue, postButton);
